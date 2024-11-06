17d1759f64b3d46d56612a62629eb42d2c7ca979
Isaah18 <0000107544066xsp@al.educacao.sp.gov.br>
2024-11-06 11:22:05 -0300

    Cards

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'K-drama (Tudo bem não ser normal)',
+    'Qual é a profissão de Moon Gang-tae?',
+    'Cuidador de pacientes psiquiátricos.'
+)
+
+criaCartao(
+    'K-drama (Tudo bem não ser normal)',
+    'Qual transtorno o irmão de Gang-tae, Sang-tae, possui?',
+    'Autismo.'
+)
+
+criaCartao(
+    'K-drama (Tudo bem não ser normal)',
+    'Qual é o nome da escritora com quem Gang-tae se envolve?',
+    'Ko Moon-young.'
+)
+
+criaCartao(
+    'K-drama (Tudo bem não ser normal)',
+    'Que tipo de livros Ko Moon-young escreve?',
+    'Livros infantis.'
+)
